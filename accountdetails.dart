80b993852e8c776df4c882299fb8a7f5c5fe54db
AqsaBano330 <aqsabano08@gmail.com>
2023-07-22 23:45:39 +0500

    yes

--- accountdetails.dart
+++ accountdetails.dart
@@ -1,11 +1,17 @@
 import 'home.dart';
+import 'dart:io';
 
 accountDetails() {
+  stdout.write("First Name:");
   print(currentUser["firstName"]);
+  stdout.write("Last Name:");
   print(currentUser["lastName"]);
+  stdout.write("Contact Number:");
   print(currentUser["phoneNo"]);
+  stdout.write("NIC Number:");
   print(currentUser['nicNo']);
+  stdout.write("Passport Number:");
   print(currentUser["passportNumber"]);
+  stdout.write("Passport Number:");
   print(currentUser["password"]);
-  print(currentUser["confirmPassword"]);
 }
