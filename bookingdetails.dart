95880657a14d64c063f6b9f2d1696832a08d9419
AqsaBano330 <aqsabano08@gmail.com>
2023-07-27 17:31:31 +0500

    dart project

--- bookingdetails.dart
+++ bookingdetails.dart
@@ -1,31 +1,41 @@
 import 'dart:io';
 
 import 'home.dart';
+import 'mainpage.dart';
 
 bookingdetails() {
   print("====================  Your Booking Details  ========================");
 
-  print("please enter booking id");
-  var bookingid123 = int.parse(stdin.readLineSync()!);
-  print(bookingid123);
-  print(currentUser["Bookingid"]);
+  bool isbookingid = false;
 
-  if (bookingid123 == currentUser["Bookingid"]) {
-    stdout.write("| Booking ID: ");
-    print("{$currentUser['Bookingid']}  |");
-    stdout.write("| Source: ");
-    print("{$currentUser['source']}   |");
-    stdout.write("| Destination: ");
-    print("{$currentUser['destination']}");
-    stdout.write("| Departure Date: ");
-    print(currentUser["ddate"]);
-    stdout.write("| Return Date: ");
-    print(currentUser["rdate"]);
-    stdout.write("| Price: ");
-    print(currentUser["price"]);
-    stdout.write("| Seat: ");
-    print(currentUser["seat"]);
-    stdout.write("| Cabin Class: ");
-    print(currentUser["cabinclass"]);
+  while (isbookingid = false) {
+    print("please enter booking id");
+    var bookingid123 = int.parse(stdin.readLineSync()!);
+    print(bookingid123);
+    print(currentUser["Bookingid"]);
+
+    if (bookingid123 == currentUser["Bookingid"]) {
+      stdout.write("| Booking ID: ");
+      print("{$currentUser['Bookingid']}  |");
+      stdout.write("| Source: ");
+      print("{$currentUser['source']}   |");
+      stdout.write("| Destination: ");
+      print("{$currentUser['destination']}");
+      stdout.write("| Departure Date: ");
+      print(currentUser["ddate"]);
+      stdout.write("| Return Date: ");
+      print(currentUser["rdate"]);
+      stdout.write("| Price: ");
+      print(currentUser["price"]);
+      stdout.write("| Seat: ");
+      print(currentUser["seat"]);
+      stdout.write("| Cabin Class: ");
+      print(currentUser["cabinclass"]);
+      print("                                            <-Back(B)");
+    }
+    if (bookingid123 == "B" || bookingid123 == "b") {
+      isbookingid = true;
+      mainpage();
+    }
   }
 }
--- endingnote.dart
+++ endingnote.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+
+import 'home.dart';
+import 'mainpage.dart';
+import 'bookingdetails.dart';
+import 'flightbooking.dart';
+
 endingnote() {
   print("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *");
   print("*                                                                 *");
@@ -7,4 +14,30 @@ endingnote() {
   print("*                                                                 *");
   print("*                                                                 *");
   print("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *");
+
+  print("Flight info:");
+  stdout.write("| Booking ID: ");
+  print(currentUser['Bookingid']);
+  stdout.write("| Source: ");
+  print(currentUser['source']);
+  stdout.write("| Destination: ");
+  print(currentUser['destination']);
+  stdout.write("| Departure Date: ");
+  print(currentUser["ddate"]);
+  stdout.write("| Return Date: ");
+  print(currentUser["rdate"]);
+  stdout.write("| Price: ");
+  print(currentUser["price"]);
+  stdout.write("| Seat: ");
+  print(currentUser["seat"]);
+  stdout.write("| Cabin Class: ");
+  print(currentUser["cabinclass"]);
+
+  print("                                                           <-Back(B)");
+
+  var endingnoteinput = stdin.readLineSync();
+
+  if (endingnoteinput == "b" || endingnoteinput == "B") {
+    mainpage();
+  }
 }
